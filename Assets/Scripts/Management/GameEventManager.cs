a4c3fc6f12c23499de114ec77070aad49a1a3db8
Blind-Guess-Senior <94767867+Blind-Guess-Senior@users.noreply.github.com>
2026-01-31 10:50:34 +0800

    feat: more event

--- Assets/Scripts/Management/GameEventManager.cs
+++ Assets/Scripts/Management/GameEventManager.cs
@@ -1,28 +1,49 @@
-using System;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class GameEventManager : MonoBehaviour
+namespace Management
 {
-    public static GameEventManager Instance { get; private set; }
-
-    public PossessionEvent onPossessionTrigger = new PossessionEvent();
-
-
-    private void Awake()
+    public class GameEventManager : MonoBehaviour
     {
-        if (Instance != null)
+        public static GameEventManager Instance { get; private set; }
+
+        public PossessionEvent onPossessionTrigger = new PossessionEvent();
+        public LevelFailEvent onLevelFail = new LevelFailEvent();
+        public LevelClearEvent onLevelClear = new LevelClearEvent();
+
+        private void Awake()
         {
-            Destroy(Instance.gameObject);
-        }
-        else
-        {
-            Instance = this;
+            if (Instance is not null && Instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                Instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
         }
     }
-}
 
-[System.Serializable]
-public class PossessionEvent : UnityEvent<GameObject, GameObject>
-{
-} // Target, Player
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <para>
+    /// first: Target
+    /// second: Player
+    /// </para>
+    [System.Serializable]
+    public class PossessionEvent : UnityEvent<GameObject, GameObject>
+    {
+    }
+
+    [System.Serializable]
+    public class LevelFailEvent : UnityEvent
+    {
+    }
+
+    [System.Serializable]
+    public class LevelClearEvent : UnityEvent
+    {
+    }
+}
